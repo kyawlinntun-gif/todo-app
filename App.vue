bd511c7f62b7f17a21a57645d4c91c2ed5026474
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2020-10-29 10:47:42 +0630

    Created a statusbar component

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="container">
+    <StatusBar color="#FFCE00"/>
+  </view>
+</template>
+
+<script>
+import StatusBar from './components/StatusBar';
+export default {
+  components: {
+    StatusBar
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  flex: 1;
+}
+</style>
---
+++ components/StatusBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <view class="statusbar" :style="{backgroundColor: color}"></view>
+</template>
+
+<script>
+export default {
+    props: [
+        'color'
+    ]
+}
+</script>
+
+<style>
+.statusbar {
+  /* background-color: #FFCE00; */
+  height: 20;
+}
+</style>
